ac8759241f510a9285de47015e9d588d97756e2f
ɴᴏᴏʙ - ᴍᴜᴋᴇsʜ <108251605+Noob-mukesh@users.noreply.github.com>
2022-10-06 21:32:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,13 @@ from os import getenv
 API_ID = int(getenv("API_ID", "7507532"))
 API_HASH = getenv("API_HASH", "c4f37d9aea37ef639f1f2da178acec3d")
 BOT_TOKEN = getenv("BOT_TOKEN", None)
-OWNER_ID = int(getenv("OWNER_ID", ""))
+OWNER_ID = int(getenv("OWNER_ID", "5207640479"))
+OWNER_USERNAME = getenv("OWNER_USERNAME","itz_mst_Boi")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT","the_support_chat")  # Your own group for support, do not add the @
+JOIN_LOGGER = getenv("JOIN_LOGGER","-1001770762410")
+EVENT_LOGS = (-100177076241)  
+LOAD = []
+NO_LOAD = ["rss", "cleaner", "connection", "math"]
+WEBHOOK = False
+INFOPIC = True
+URL = None
